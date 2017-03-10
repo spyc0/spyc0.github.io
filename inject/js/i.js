f893cb1e95e5213dc86e745dc1ed65d97b6a1ddb
spyc0 <spyc0@users.noreply.github.com>
2017-03-10 05:36:19 -0600

    Create i.js

---
+++ inject/js/i.js
@@ -0,0 +1,8 @@
+
+javascript:(function(){
+  var style = document.createElement('style');
+  var styleContent = document.createTextNode('body{background-color:red}');
+  style.appendChild(styleContent );
+  var caput = document.getElementsByTagName('head');
+  caput[0].appendChild(style);
+})();
